b124a9e4718922bf8a728132b3209035e6ca4631
Mitchell Henke <mitchh23@gmail.com>
2015-10-17 10:09:20 -0500

    remove tzdata application

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule CircuitBreaking.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :tzdata]]
+    [applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:

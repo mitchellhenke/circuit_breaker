a9fa6046187693eb3a47111a26996511002e8950
Mitchell Henke <mitchh23@gmail.com>
2015-10-17 10:28:54 -0500

    don't "use HTTPoison"

--- lib/itunes_searcher.ex
+++ lib/itunes_searcher.ex
@@ -1,5 +1,4 @@
 defmodule CircuitBreaking.ItunesSearcher do
-  use HTTPoison.Base
   def search_music(query) do
     default_params
     |> Map.put(:term, query)
@@ -8,7 +7,7 @@ defmodule CircuitBreaking.ItunesSearcher do
 
   defp make_request(params) do
     url = "#{search_url()}?#{URI.encode_query(params)}"
-    case get(url) do
+    case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         Poison.decode(body)
       {:ok, %HTTPoison.Response{status_code: code, body: body}} ->
